3e47e71f73fed4bee8daef0bc18ad8db1c5dd002
Mohammad Sadeq Khandakar <msadeq27@yahoo.com>
2023-10-30 06:33:48 -0400

    Player tap box to reveal image is working. Currently developing the game win conditions.

--- app/src/main/java/com/example/mskassignment_2/GameActivity.java
+++ app/src/main/java/com/example/mskassignment_2/GameActivity.java
@@ -1,18 +1,61 @@
 package com.example.mskassignment_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
 
-    enum Players{
-        Player_X,
-        Player_Y
-    }
+    private Drawable xPlayer;
+    private Drawable oPlayer;
+
+    private static int total_moves = 9;
+
+    public static int moveCounter = 0;
+    private ImageView[] boxes = new ImageView[9];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        xPlayer = ResourcesCompat.getDrawable(getResources(), R.drawable.token_x, null);
+        oPlayer = ResourcesCompat.getDrawable(getResources(), R.drawable.token_o, null);
+
+    }
+
+    public Drawable ShowPlayer(int count, ImageView image){
+
+        if(count % 2 == 0){
+            image.animate().translationYBy(50f);
+            return xPlayer;
+        }
+
+        else{
+            image.animate().translationYBy(50f);
+            return oPlayer;
+        }
+    }
+
+    /*
+        Instantiates an ImageView at every click
+     */
+    public void playerTap(View view){
+        moveCounter++;
+        ImageView imgPlayer = (ImageView) view;
+        int tappedBox = Integer.parseInt(imgPlayer.getTag().toString());
+
+        imgPlayer.setTranslationY(-100f);
+
+        imgPlayer.setImageDrawable(ShowPlayer(moveCounter, imgPlayer));
+
+
+
+        Log.i("tappedBox", imgPlayer.getTag().toString());
     }
 }
